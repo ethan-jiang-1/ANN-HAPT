18f680c480091122f47e356ce70ddf16bfb3bf7b
ethanmac <ethanmac@mac.com>
2020-10-24 19:52:19 +0800

    1 or 2 layers

--- port_1_xnn/ps_save_xnn_all.py
+++ port_1_xnn/ps_save_xnn_all.py
@@ -34,22 +34,68 @@ ry_train = y_train
 rx_test = x_test
 ry_test = y_test
 
-max_iter = 400
-
 
 model = MLPClassifier(activation='relu', alpha=0.0001, batch_size='auto', beta_1=0.9,
        beta_2=0.999, early_stopping=False, epsilon=1e-08,
-       hidden_layer_sizes=(388), learning_rate='constant',
-       learning_rate_init=0.001, max_iter=max_iter, momentum=0.9,
+       hidden_layer_sizes=(), learning_rate='constant',
+       learning_rate_init=0.001, max_iter=200, momentum=0.9,
        nesterovs_momentum=True, power_t=0.5, random_state=None,
        shuffle=True, solver='adam', tol=0.0001, validation_fraction=0.1,
-       verbose=True, warm_start=False)
+       verbose=False, warm_start=False)
 
 model.fit(rx_train, ry_train)
 label=model.predict(rx_test)
 
 
-print(classification_report(label, ry_test, digits=4,  target_names=[lb for lb in labels.values()]))
+cdr = os.path.dirname(__file__)
+os.chdir(cdr)
+
+import pickle
+pkl_filename = "ps_model.pkl"
+with open(pkl_filename, 'wb') as file:
+    pickle.dump(model, file)
+print("train model saved: {}".format(pkl_filename))
+
+from sklearn_porter import Porter
+
+porter = Porter(model, language='java')
+output = porter.export()
+cpt_filename = "MLPClassifier.java"
+with open(cpt_filename, 'w') as file:
+    n = file.write(output)
+    print("tran model save in data: {} len: {}".format(cpt_filename, len(output)))
+
+'''
+            for (int i =0; i< rx_test_num; i++) {
+                // Features:
+                double[] features = rx_test[i];
+                int estimation = clf.predict(features);
+                System.out.println(estimation);            
+             }
+'''
+
+cpt_testfilename = "MLPClassifier.dat"
+if os.path.isfile(cpt_testfilename):
+    os.unlink(cpt_testfilename)
+
+print("prepare predit data")
+
+with open(cpt_testfilename, 'w+') as file:
+    file.write("int rx_test_num = {};\n".format(len(rx_test)))
+    file.write("double rx_test[][] = ")
+    file.write(" {\n")
+    for i in range(0, len(rx_test)):
+        test_case = rx_test[i]
+        file.write("{")
+        for j in range(0, len(test_case)):
+            if j == len(test_case) - 1:
+                file.write("{}".format(test_case[j]))
+            else:
+                file.write("{},".format(test_case[j]))
+        file.write("},\n")
+    file.write("};\n")
+
+print(classification_report(label, ry_test, target_names=[lb for lb in labels.values()]))
 
 conf_mat = confusion_matrix(label, ry_test)
 
@@ -69,18 +115,6 @@ plt.title('Confusion Matrix')
 _ = plt.xticks(range(6), [lb for lb in labels.values()], rotation=90)
 _ = plt.yticks(range(6), [lb for lb in labels.values()])
 plt.show()
-
-
-def predict_meaure(name, model, rx_test):
-    import time
-    print("check predict performance {}...".format(name))
-    begin = time.time()
-    model.predict(rx_test)
-    delta = time.time() - begin
-    print("check predict perform time_elapse: {:.2f} sec on total {} samples, {:.4f} msec per prediction ".format(delta, len(rx_test), delta*1000/len(rx_test)))
-
-
-predict_meaure("rfc", model, rx_test)
 
 from s_inspect import inspect_xnn
 inspect_xnn(model)
